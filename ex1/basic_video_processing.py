b7a40dde6229e33d2e504f8d1fb33fb3cd67cf6a
yuval naor <yuvalnaor@mail.tau.ac.il>
2023-03-25 16:23:32 +0300

    started ex1

--- ex1/basic_video_processing.py
+++ ex1/basic_video_processing.py
@@ -1,13 +1,14 @@
 """Basic Video Processing methods."""
 import os
 import cv2
+import numpy as np
 
 
 # Replace ID1 and ID2 with your IDs.
 ID1 = '123456789'
 ID2 = '987654321'
 
-INPUT_VIDEO = 'atrium.avi'
+INPUT_VIDEO = '/home/yuval/repos/VP2023/ex1/atrium.avi'
 GRAYSCALE_VIDEO = f'{ID1}_{ID2}_atrium_grayscale.avi'
 BLACK_AND_WHITE_VIDEO = f'{ID1}_{ID2}_atrium_black_and_white.avi'
 SOBEL_VIDEO = f'{ID1}_{ID2}_atrium_sobel.avi'
@@ -42,16 +43,28 @@ def convert_video_to_grayscale(input_video_path: str,
     Args:
         input_video_path: str. Path to input video.
         output_video_path: str. Path to output video.
-
-    Additional References:
-    (1) What are fourcc parameters:
-    https://docs.microsoft.com/en-us/windows/win32/medfound/video-fourccs
-
     """
-    """INSERT YOUR CODE HERE.
-    REMOVE THE pass KEYWORD AND IMPLEMENT YOUR OWN CODE.
-    """
-    pass
+    cap = cv2.VideoCapture(input_video_path)
+    out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'MJPG'),
+                          cap.get(cv2.CAP_PROP_FPS),
+                          (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                           int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))),
+                          isColor=False)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        out.write(gray_frame)
+
+        cv2.imshow('frame', gray_frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
 
 def convert_video_to_black_and_white(input_video_path: str,
@@ -71,15 +84,42 @@ def convert_video_to_black_and_white(input_video_path: str,
         input_video_path: str. Path to input video.
         output_video_path: str. Path to output video.
 
-    Additional References:
-    (1) What are fourcc parameters:
-    https://docs.microsoft.com/en-us/windows/win32/medfound/video-fourccs
-
     """
-    """INSERT YOUR CODE HERE.
-        REMOVE THE pass KEYWORD AND IMPLEMENT YOUR OWN CODE.
-        """
-    pass
+    cap = cv2.VideoCapture(input_video_path)
+
+    # Get input video parameters
+    parameters = get_video_parameters(cap)
+
+    # Define output video codec and initialize output video writer
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    out = cv2.VideoWriter(output_video_path, fourcc, parameters['fps'], (parameters['width'], parameters['height']))
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Convert frame to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Apply Otsu's thresholding to get binary image
+        _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+        # Convert binary image to RGB
+        binary = cv2.cvtColor(binary, cv2.COLOR_GRAY2RGB)
+
+        # Write the new frame to the output video
+        out.write(binary)
+
+        # Display the new frame
+        cv2.imshow('frame', binary)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release everything
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
 
 def convert_video_to_sobel(input_video_path: str,
@@ -99,15 +139,58 @@ def convert_video_to_sobel(input_video_path: str,
         input_video_path: str. Path to input video.
         output_video_path: str. Path to output video.
 
-    Additional References:
-    (1) What are fourcc parameters:
-    https://docs.microsoft.com/en-us/windows/win32/medfound/video-fourccs
-
     """
     """INSERT YOUR CODE HERE.
         REMOVE THE pass KEYWORD AND IMPLEMENT YOUR OWN CODE.
         """
-    pass
+    cap = cv2.VideoCapture(input_video_path)
+
+    # Get the frames per second (fps) of the input video
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    # Get the width and height of the input video
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # Create a VideoWriter object for the output video
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height), isColor=False)
+
+    while True:
+        # Read a frame from the input video
+        ret, frame = cap.read()
+
+        # Break the loop if we have reached the end of the input video
+        if not ret:
+            break
+
+        # Convert the frame to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Compute the Sobel gradient in the x and y directions
+        sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)
+        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)
+
+        # Compute the absolute value of the gradient in each direction
+        abs_sobelx = cv2.convertScaleAbs(sobelx)
+        abs_sobely = cv2.convertScaleAbs(sobely)
+
+        # Combine the x and y gradients using a weighted sum
+        alpha = 0.5
+        sobel = cv2.addWeighted(abs_sobelx, alpha, abs_sobely, 1-alpha, 0)
+
+        # Convert the Sobel image to an integer data type
+        sobel = sobel.astype(np.uint8)
+
+        # Threshold the Sobel image using Otsu's method
+        _, binary = cv2.threshold(sobel, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        # Write the binary image to the output video file
+        out.write(binary)
+
+    # Release the video file readers and writers
+    cap.release()
+    out.release()
 
 
 def main():
